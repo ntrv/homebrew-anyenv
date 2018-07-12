0629bd6c72fce4d875643e7687b8e6a53da169a9
ntrv <hydrangea56@me.com>
2018-07-12 20:20:39 +0900

    Passed brew audit --strict

--- anyenv-update.rb
+++ anyenv-update.rb
@@ -1,14 +1,18 @@
 class AnyenvUpdate < Formula
-    desc "An anyenv subcommand to update all **env and all plugins"
-    homepage "https://github.com/znz/anyenv-update"
-    url "https://github.com/znz/anyenv-update/archive/b6cefdb4aeaf930a9e576c515cd557f3acf3ab91.zip"
-    sha256 "e3bb77358deb67d67a67df834342d776c4da0545793e72ebe71a2948985a5ba0"
-    version "b6cefdb"
-    head "https://github.com/znz/anyenv-update.git"
+  desc "Anyenv subcommand to update all **env and all plugins"
+  homepage "https://github.com/znz/anyenv-update"
+  url "https://github.com/znz/anyenv-update/archive/b6cefdb4aeaf930a9e576c515cd557f3acf3ab91.tar.gz"
+  version "b6cefdb"
+  sha256 "e3bb77358deb67d67a67df834342d776c4da0545793e72ebe71a2948985a5ba0"
+  head "https://github.com/znz/anyenv-update.git"
 
-    depends_on "anyenv"
+  depends_on "anyenv"
 
-    def install
-        prefix.install Dir["*"]
-    end
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    shell_output(%Q(test -x "#{bin}/anyenv-update"))
+  end
 end
--- anyenv.rb
+++ anyenv.rb
@@ -1,30 +1,30 @@
 class Anyenv < Formula
-    desc "[anyenv] all in one for **env"
-    homepage "https://github.com/riywo/anyenv"
-    url "https://github.com/riywo/anyenv/archive/3cb8ad1b0dfd89ed5a53fcc9e076b371f6baabfc.zip"
-    sha256 "5bfa5785cdfcf5f6131cd22e1e8011ec41519b00e5e1c586c8bd00e45a054df1"
-    version "3cb8ad1"
-    head "https://github.com/riywo/anyenv.git"
+  desc "[anyenv] all in one for **env"
+  homepage "https://github.com/riywo/anyenv"
+  url "https://github.com/riywo/anyenv/archive/3cb8ad1b0dfd89ed5a53fcc9e076b371f6baabfc.tar.gz"
+  version "3cb8ad1"
+  sha256 "5bfa5785cdfcf5f6131cd22e1e8011ec41519b00e5e1c586c8bd00e45a054df1"
+  head "https://github.com/riywo/anyenv.git"
 
-    def install
-        inreplace "libexec/anyenv", %(ANYENV_ROOT="${HOME}/.anyenv"), %(ANYENV_ROOT="#{prefix}")
-        prefix.install Dir["*"]
+  def install
+    inreplace "libexec/anyenv", 'ANYENV_ROOT="${HOME}/.anyenv"', %Q(ANYENV_ROOT="#{prefix}")
+    prefix.install Dir["*"]
 
-        if build.with? "completions"
-            bash_completion.install "completions/anyenv.bash"
-            fish_completion.install "completions/anyenv.fish"
-            zsh_completion.install "completions/anyenv.zsh"
-        end
+    if build.with? "completions"
+      bash_completion.install "completions/anyenv.bash"
+      fish_completion.install "completions/anyenv.fish"
+      zsh_completion.install "completions/anyenv.zsh"
     end
+  end
 
-    def caveats; <<~EOS
-        To enable anyenv, add below code to your ~/.*shrc:
-            export PATH="$(anyenv root)/bin:$PATH"
-            eval "$(anyenv init -)"
-        EOS
-    end
+  def caveats; <<~EOS
+    To enable anyenv, add below code to your ~/.*shrc:
+        export PATH="$(anyenv root)/bin:$PATH"
+        eval "$(anyenv init -)"
+    EOS
+  end
 
-    test do
-        shell_output(%(eval "$(#{bin}/anyenv init -)" && anyenv version))
-    end
+  test do
+    shell_output(%Q(eval "$(#{bin}/anyenv init -)" && anyenv version))
+  end
 end
