816a9e05bc36b202b864df4142563443593f497e
ntrv <hydrangea56@me.com>
2018-07-12 20:39:49 +0900

    HeadOnly formulae

--- anyenv-git.rb
+++ anyenv-git.rb
@@ -1,9 +1,6 @@
 class AnyenvGit < Formula
   desc "`anyenv git` command to run `git` in all repos of **env and plugins"
   homepage "https://github.com/znz/anyenv-git"
-  url "https://github.com/znz/anyenv-git/archive/e091c9428c60af58cc071f2ec84a0f8276e809db.tar.gz"
-  version "e091c94"
-  sha256 "a8e8828e1a49eb3d47966375363877f756d6eb69a13fb99a7fd5d13cd54937b1"
   head "https://github.com/znz/anyenv-git.git"
 
   depends_on "anyenv"
--- anyenv-update.rb
+++ anyenv-update.rb
@@ -1,9 +1,6 @@
 class AnyenvUpdate < Formula
   desc "Anyenv subcommand to update all **env and all plugins"
   homepage "https://github.com/znz/anyenv-update"
-  url "https://github.com/znz/anyenv-update/archive/b6cefdb4aeaf930a9e576c515cd557f3acf3ab91.tar.gz"
-  version "b6cefdb"
-  sha256 "e3bb77358deb67d67a67df834342d776c4da0545793e72ebe71a2948985a5ba0"
   head "https://github.com/znz/anyenv-update.git"
 
   depends_on "anyenv"
